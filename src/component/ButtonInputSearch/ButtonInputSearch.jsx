6afb92cd6be32a5ef1944f4bc90cfdc9ccd0f2dd
Nhuttruong61 <Nhuttruong2261@gmail.com>
2023-04-15 21:09:23 +0700

    update hearder

---
+++ src/component/ButtonInputSearch/ButtonInputSearch.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { SearchOutlined } from "@ant-design/icons";
+import InputComponent from "../InputComponet/InputComponent";
+import ButtonComponent from "../ButtonComponent/ButtonComponent";
+
+const ButttonInputSearch = (props) => {
+  const {
+    size,
+    placeholder,
+    textbutton,
+    bordered,
+    backgroundColorInput = "#fff",
+    backgroundColorButton = "#fff",
+    colorButton = "#000",
+  } = props;
+
+  return (
+    <div style={{ display: "flex" }}>
+      <InputComponent
+        size={size}
+        placeholder={placeholder}
+        bordered={bordered}
+        style={{ backgroundColor: backgroundColorInput, borderRadius: 0 }}
+        {...props}
+      />
+      <ButtonComponent
+        size={size}
+        styleButton={{
+          background: backgroundColorButton,
+          border: !bordered && "none",
+          borderRadius: 0,
+        }}
+        icon={<SearchOutlined color={colorButton} style={{ color: "#000" }} />}
+        textbutton={textbutton}
+        styleTextButton={{ color: colorButton }}
+      />
+    </div>
+  );
+};
+
+export default ButttonInputSearch;
--- src/component/HeaderComponet/HeaderComponet.jsx
+++ src/component/HeaderComponet/HeaderComponet.jsx
@@ -6,21 +6,30 @@ import {
   WrapperTextHeaderSmall,
 } from "./style";
 import { Badge, Col } from "antd";
-import Search from "antd/es/transfer/search";
+
 import {
   UserOutlined,
   CaretDownOutlined,
   ShoppingCartOutlined,
 } from "@ant-design/icons";
+import ButttonInputSearch from "../ButtonInputSearch/ButtonInputSearch";
 function HeaderComponet() {
   return (
-    <div style={{  heiht: '100%', width: '100%', background: 'rgb(26, 148, 255)', display: 'flex', justifyContent: 'center' }}>
+    <div
+      style={{
+        heiht: "100%",
+        width: "100%",
+        background: "rgb(26, 148, 255)",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
       <WrapperHeader>
         <Col span={5}>
-          <WrapperTextHeader to='/'>ECOMMERCE</WrapperTextHeader>
+          <WrapperTextHeader to="/">ECOMMERCE</WrapperTextHeader>
         </Col>
         <Col span={12}>
-          <Search
+          <ButttonInputSearch
             size="large"
             bordered={false}
             textbutton="Tìm kiếm"
@@ -28,7 +37,10 @@ function HeaderComponet() {
             // onChange={onSearch}
           />
         </Col>
-        <Col span={6} style={{ display: 'flex', gap: '54px', alignItems: 'center' }}>
+        <Col
+          span={6}
+          style={{ display: "flex", gap: "54px", alignItems: "center" }}
+        >
           <WrapperHeaderAccout>
             <UserOutlined style={{ fontSize: "30px" }} />
 
@@ -41,10 +53,12 @@ function HeaderComponet() {
             </div>
           </WrapperHeaderAccout>
           <div>
-          <Badge  size="small">
-                <ShoppingCartOutlined style={{ fontSize: '30px', color: '#fff' }} />
-              </Badge>
-              <WrapperTextHeaderSmall>Giỏ hàng</WrapperTextHeaderSmall>
+            <Badge size="small">
+              <ShoppingCartOutlined
+                style={{ fontSize: "30px", color: "#fff" }}
+              />
+            </Badge>
+            <WrapperTextHeaderSmall>Giỏ hàng</WrapperTextHeaderSmall>
           </div>
         </Col>
       </WrapperHeader>
