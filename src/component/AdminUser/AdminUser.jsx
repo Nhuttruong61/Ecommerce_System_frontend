ab9cb92cfdb963b8051431dad49e4c18f823b9b6
Nhuttruong61 <Nhuttruong2261@gmail.com>
2023-04-28 16:30:59 +0700

    xử lý AdminUser

--- src/component/AdminUser/AdminUser.jsx
+++ src/component/AdminUser/AdminUser.jsx
@@ -1,28 +1,424 @@
-import React from "react";
-import { WrapperHeader } from "./style";
-import { Button } from "antd";
+import React, { useEffect, useRef, useState } from "react";
+import { WrapperHeader, WrapperUploadFile } from "./style";
+import { Button, Form, Space } from "antd";
 import TableComponent from "../TableComponent/TableComponent";
 import { PlusOutlined } from "@ant-design/icons";
+import InputComponet from "../InputComponet/InputComponent";
+import DrawerComponent from "../DrawerComponet/DrawerComponet";
+import Loading from "../LoadingComponet/LoadingComponet";
+import ModalComponent from "../ModalComponent/ModalComponent";
+import { getBase64 } from "../../utils";
+import { useMutationHooks } from "../../hooks/useMutationHook";
+import * as message from "../../component/Mesage/Message";
+import { useQuery } from "react-query";
+import { useSelector } from "react-redux";
+import * as UserService from "../../services/UserService";
+import {
+  DeleteOutlined,
+  EditOutlined,
+  SearchOutlined,
+} from "@ant-design/icons";
 
 function AdminUser() {
+  const [rowSelected, setRowSelected] = useState("");
+  const [isOpenDrawer, setIsOpenDrawer] = useState(false);
+  const [isLoadingUpdate, setIsLoadingUpdate] = useState(false);
+  const [isModalOpenDelete, setIsModalOpenDelete] = useState(false);
+  const user = useSelector((state) => state && state.user);
+  const [searchedColumn, setSearchedColumn] = useState("");
+  const searchInput = useRef(null);
+  const [stateUserDetails, setStateUserDetails] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    isAdmin: false,
+  });
+
+  const mutationUpdate = useMutationHooks((data) => {
+    const { id, token, ...rests } = data;
+    const res = UserService.updateUser(id,  { ...rests },token);
+    return res;
+  });
+
+  const mutationDeleted = useMutationHooks((data) => {
+    const { id, token } = data;
+    const res = UserService.deleteUser(id, token);
+    return res;
+  });
+
+  const getAllUser = async () => {
+    const res = await UserService.getAllUser();
+    return res;
+  };
+  const fetchDetailsUser= async (rowSelected) => {
+    const res = await UserService.getDetailsUser(rowSelected);
+    if (res && res.data) {
+      setStateUserDetails({
+        name: res && res.data && res.data.name,
+        email: res && res.data && res.data.email,
+        phone: res && res.data && res.data.phone,
+        isAdmin: res && res.data && res.data.isAdmin,
+      });
+    }
+    setIsLoadingUpdate(false);
+  };
+
+  useEffect(() => {
+    form.setFieldsValue({
+      name: stateUserDetails.name,
+      email: stateUserDetails.email,
+      phone: stateUserDetails.phone,
+      isAdmin: stateUserDetails.isAdmin,
+    });
+  }, [form, stateUserDetails]);
+
+  useEffect(() => {
+    if (rowSelected) {
+      fetchDetailsUser(rowSelected);
+    }
+  }, [rowSelected]);
+  console.log("stateUserdetail", stateUserDetails);
+  const handleDetailsProduct = () => {
+    setIsOpenDrawer(true);
+  };
+
+  const {
+    data: dataUpdated,
+    isLoading: isLoadingUpdated,
+    isSuccess: isSuccessUpdated,
+    isError: isErrorUpdated,
+  } = mutationUpdate;
+  const {
+    data: dataDeleted,
+    isLoading: isLoadingDeleted,
+    isSuccess: isSuccessDeleted,
+    isError: isErrorDeleted,
+  } = mutationDeleted;
+  const queryUser = useQuery(["user"], getAllUser);
+  const { isLoading: isLoadingUsers, data: users } = queryUser;
+
+  const [form] = Form.useForm();
+
+  useEffect(() => {
+    if (isSuccessDeleted && dataDeleted && dataDeleted.status === "OK") {
+      message.success();
+      handleCancelDelete();
+    } else if (isErrorDeleted) {
+      message.error();
+    }
+  }, [isSuccessDeleted]);
+
+  const handleCloseDrawer = () => {
+    setIsOpenDrawer(false);
+    setStateUserDetails({
+      name: "",
+      email: "",
+      phone: "",
+      isAdmin: false,
+    });
+    form.resetFields();
+  };
+  useEffect(() => {
+    if (isSuccessUpdated && dataUpdated && dataUpdated.status === "OK") {
+      message.success();
+      handleCloseDrawer();
+    } else if (isErrorUpdated) {
+      message.error();
+    }
+  }, [isSuccessUpdated]);
+  let dataTable = [];
+  if (users && users.data && users.data.length > 0) {
+    dataTable = users.data.map((user) => {
+      return { ...user, key: user._id, isAdmin: user.isAdmin ? "TRUE": "FALSE" };
+    });
+  }
+
+  const renderAction = () => {
+    return (
+      <div>
+        <DeleteOutlined
+          style={{ color: "red", fontSize: "30px", cursor: "pointer" }}
+          onClick={() => setIsModalOpenDelete(true)}
+        />
+        <EditOutlined
+          style={{ color: "orange", fontSize: "30px", cursor: "pointer" }}
+          onClick={handleDetailsProduct}
+        />
+      </div>
+    );
+  };
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchedColumn(dataIndex);
+  };
+  const handleReset = (clearFilters) => {
+    clearFilters();
+  };
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+      close,
+    }) => (
+      <div
+        style={{
+          padding: 8,
+        }}
+        onKeyDown={(e) => e.stopPropagation()}
+      >
+        <InputComponet
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{
+            marginBottom: 8,
+            display: "block",
+          }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}
+          >
+            close
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined
+        style={{
+          color: filtered ? "#1890ff" : undefined,
+        }}
+      />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(function () {
+          if (searchInput.current) {
+            searchInput.current.select();
+          }
+        }, 100);
+      }
+    },
+
+  });
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      sorter: (a, b) => a.name.length - b.name.length,
+      ...getColumnSearchProps("name"),
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      sorter: (a, b) => a.email.length - b.email.length,
+      ...getColumnSearchProps("email"),
+    },
+    {
+      title: "Admin",
+      dataIndex: "isAdmin",
+      filters: [
+        {
+          text: "True",
+          value: true,
+        },
+        {
+          text: "False",
+          value: false,
+        },
+      ],
+    },
+    {
+      title: "Phone",
+      dataIndex: "phone",
+      sorter: (a, b) => a.phone - b.phone,
+      ...getColumnSearchProps("phone"),
+    },
+    {
+      title: "Action",
+      dataIndex: "action",
+      render: renderAction,
+    },
+  ];
+  const handleCancelDelete = () => {
+    setIsModalOpenDelete(false);
+  };
+  const handleDeleteUser = () => {
+    mutationDeleted.mutate(
+      { id: rowSelected, token: user && user.access_token },
+      {
+        onSettled: () => {
+          queryUser.refetch();
+        },
+      }
+    );
+    console.log("delete");
+  };
+
+const handleOnchangeDetails = (e) => {
+    setStateUserDetails({
+      ...stateUserDetails,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+
+  const onUpdateUser = () => {
+    mutationUpdate.mutate(
+      {
+        id: rowSelected,
+        token: user && user.access_token,
+        ...stateUserDetails,
+      },
+      {
+        onSettled: () => {
+          queryUser.refetch();
+        },
+      }
+    );
+  };
   return (
     <div>
       <WrapperHeader>Quản lý người dùng</WrapperHeader>
-      <div style={{ marginTop: "10px" }}>
-        <Button
-          style={{
-            height: "150px",
-            width: "150px",
-            borderRadius: "6px",
-            borderStyle: "dashed",
-          }}
-        >
-          <PlusOutlined style={{ fontSize: "60px" }} />
-        </Button>
-      </div>
+      
       <div style={{ marginTop: "20px" }}>
-        <TableComponent />
+        <TableComponent
+          columns={columns}
+          isLoading={isLoadingUsers}
+          data={dataTable}
+          onRow={(record, rowIndex) => {
+            return {
+              onClick: (event) => {
+                setRowSelected(record._id);
+              }, // click row
+            };
+          }}
+        />
       </div>
+      <DrawerComponent
+        title="Chi tiết người dùng"
+        isOpen={isOpenDrawer}
+        onClose={() => setIsOpenDrawer(false)}
+        width="80%"
+      >
+        <Loading isLoading={isLoadingUpdate || isLoadingUpdated}>
+          <Form
+            name="basic"
+            labelCol={{
+              span: 6,
+            }}
+            wrapperCol={{
+              span: 18,
+            }}
+            style={{
+              maxWidth: 600,
+            }}
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onUpdateUser}
+            autoComplete="on"
+            form={form}
+          >
+            <Form.Item
+              label="Name"
+              name="name"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your name!",
+                },
+              ]}
+            >
+              <InputComponet
+                value={stateUserDetails.name}
+                onChange={handleOnchangeDetails}
+                name="name"
+              />
+            </Form.Item>
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your  email!",
+                },
+              ]}
+            >
+              <InputComponet
+                value={stateUserDetails.email}
+                onChange={handleOnchangeDetails}
+                name="email"
+              />
+            </Form.Item>
+            <Form.Item
+              label="Phone"
+              name="phone"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your  count phone!",
+                },
+              ]}
+            >
+              <InputComponet
+                value={stateUserDetails.phone}
+                onChange={handleOnchangeDetails}
+                name="phone"
+              />
+            </Form.Item>
+            <Form.Item wrapperCol={{ offset: 20, span: 16 }}>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </Loading>
+      </DrawerComponent>
+      <ModalComponent
+        title="Xóa người dùng"
+        open={isModalOpenDelete}
+        onCancel={handleCancelDelete}
+        onOk={handleDeleteUser}
+      >
+        <Loading isLoading={isLoadingDeleted}>
+          <div>Bạn có chắc xóa tài khoản này không?</div>
+        </Loading>
+      </ModalComponent>
     </div>
   );
 }
